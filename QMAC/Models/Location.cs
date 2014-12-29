6186a8c59f9eb373136d638bc3451e8e1db835d7
RandomlyKnighted <thughes@randomlyknighted.com>
2014-12-29 10:48:13 -0600

    Updated school listings and switched textbox to listbox

--- QMAC/Models/Location.cs
+++ QMAC/Models/Location.cs
@@ -23,16 +23,18 @@ namespace QMAC.Models
         {
             List<string> site_list = new List<string>();
             site_list.Add("Collinsville High School");
-            site_list.Add("Crossville Elementary");
+            site_list.Add("Crossville Elem School");
+            site_list.Add("Crossville Middle School");
             site_list.Add("Crossville High School");
             site_list.Add("Fyffe High School");
             site_list.Add("Geraldine High School");
             site_list.Add("Henagar School");
             site_list.Add("Ider High School");
+            site_list.Add("Moon Lake Elem School");
             site_list.Add("Plainview High School");
             site_list.Add("Ruhama School");
-            site_list.Add("South DeKalb Primary");
             site_list.Add("Sylvania High School");
+            site_list.Add("Tech School (DCTC)");
             site_list.Add("Valley Head High School");
             return site_list;
         }
