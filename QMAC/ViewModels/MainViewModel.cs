ef66a371412949e458eb13daffac35f19046fb44
RandomlyKnighted <thughes@randomlyknighted.com>
2014-12-29 15:09:01 -0600

    Improving the ViewModel for the ListView

--- QMAC/ViewModels/MainViewModel.cs
+++ QMAC/ViewModels/MainViewModel.cs
@@ -2,11 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
 using System.Windows.Input;
-using System.Windows.Threading;
 
 namespace QMAC.ViewModels
 {
--- QMAC/ViewModels/ViewModelBase.cs
+++ QMAC/ViewModels/ViewModelBase.cs
@@ -6,9 +6,79 @@ using System.Text;
 
 namespace QMAC.ViewModels
 {
-    class ViewModelBase : INotifyPropertyChanged
+    class ViewModelBase<T> : INotifyPropertyChanged
     {
+        /*
+         * http://perezgb.com/2010/01/02/mvvm-multiselect-listbox
+         * 
+         * /
+
+        private bool isSelected;
+        private T item;
         public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler SelectionChanged;
+
+        /*
+         *  This property was created for use with ListViews. It's primary
+         *  purpose is to help aid in the creation of a databinding that allows
+         *  for multiple selections in a ListView.
+         */
+        public bool IsSelected
+        {
+            get { return IsSelected; }
+            set
+            {
+                if (value == isSelected)
+                {
+                    return;
+                }
+
+                isSelected = value;
+                OnPropertyChanged("isSelected");
+                OnSelectionChanged();
+            }
+        }
+
+        /*
+         *  This property was also created for use with ListViews. It's primary
+         *  purpose is to help aid in the creation of a databinding that allows
+         *  for multiple selections in a ListView.
+         */
+        public T Item
+        {
+            get { return Item; }
+            set
+            {
+                if (value.Equals(item))
+                {
+                    return;
+                }
+
+                item = value;
+                OnPropertyChanged("Item");
+            }
+        }
+
+        /*
+         *  This constructor was created for use with ListViews. It's primary
+         *  purpose is to help aid in the creation of a databinding that allows
+         *  for multiple selections in a ListView.
+         */
+        public ViewModelBase(T item)
+            : this(false, item)
+        {
+        }
+
+        /*
+         *  This constructor was also created for use with ListViews. It's primary
+         *  purpose is to help aid in the creation of a databinding that allows
+         *  for multiple selections in a ListView.
+         */
+        public ViewModelBase(bool selected, T item)
+        {
+            this.isSelected = selected;
+            this.item = item;
+        }
 
         protected virtual void OnPropertyChanged(string propertyName)
         {
@@ -17,6 +87,15 @@ namespace QMAC.ViewModels
             {
                 var e = new PropertyChangedEventArgs(propertyName);
                 handler(this, e);
+            }
+        }
+
+        protected virtual void OnSelectionChanged()
+        {
+            EventHandler changed = SelectionChanged;
+            if (changed != null)
+            {
+                changed(this, EventArgs.Empty);
             }
         }
     }
